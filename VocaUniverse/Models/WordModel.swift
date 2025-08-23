cfe5b2599eeb897663566b0c9480242fc4dff65f
Jini <yjkwon424@gmail.com>
2025-08-23 21:50:05 +0900

    Feat: 단어 데이터셋 추가

--- VocaUniverse/Models/WordModel.swift
+++ VocaUniverse/Models/WordModel.swift
@@ -5,3 +5,20 @@
 //  Created by 제하맥 on 8/23/25.
 //
 
+import Foundation
+import SwiftData
+
+///영단어 데이터 모델
+@Model
+final class WordModel {
+    @Attribute(.unique) var id: UUID
+    var wordEng: String
+    var meanKor: String
+
+    
+    init(id: UUID = UUID(), wordEng: String, meanKor: String) {
+        self.id = id
+        self.wordEng = wordEng
+        self.meanKor = meanKor
+    }
+}
