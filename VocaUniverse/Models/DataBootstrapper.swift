d1b8329af68f47f7794a3cee0151f5bf666f5f4b
Jini <yjkwon424@gmail.com>
2025-08-23 23:51:37 +0900

    feat: SwiftData 연결

---
+++ VocaUniverse/Models/DataBootstrapper.swift
@@ -0,0 +1,119 @@
+//
+//  DataBootstrapper.swift
+//  VocaUniverse
+//
+//  Created by Jini on 8/23/25.
+//
+
+import Foundation
+import SwiftUI
+import SwiftData
+import os.log
+
+struct DataBootstrapper {
+    
+    private let context: ModelContext
+    
+    private let fileNames = [
+        "GRE prep",
+        "HighSchool Voca",
+        "Instant Vocabulary",
+        "Merriam Webster",
+        "Reading & Vocabulary",
+        "TOEFL",
+        "Vocabulary 22000",
+        "Vocabulary 22000 Ⅱ",
+        "Word Power"
+    ]
+    
+    private let fileExt = "json"
+    
+    private let logger = Logger.init(subsystem: Bundle.main.bundleIdentifier ?? "name.jehaHwang.VocaUniverse", category: "DataBootstrapper")
+    
+    public init(context: ModelContext) {
+        self.context = context
+    }
+    
+    func bootstrap() throws {
+        
+        logger.info("started to bootstrap data")
+        
+        for fileName in fileNames {
+            loadAndInsertData(fileName: fileName)
+        }
+        
+        do {
+            try context.save()
+            logger.info("All vocabulary data saved successfully.")
+        } catch {
+            logger.error("Failed to save context: \(error.localizedDescription)")
+            throw error
+        }
+    }
+    
+    private func loadAndInsertData(fileName: String) {
+        guard let fileLocation = Bundle.main.url(forResource: fileName, withExtension: fileExt) else {
+            logger.error("File not found: \(fileName).json")
+            return
+        }
+        
+        do {
+            let data = try Data(contentsOf: fileLocation)
+            let wordList: [WordJSON] = try JSONDecoder().decode([WordJSON].self, from: data)
+            //return try JSONDecoder().decode(type, from: data)
+            
+            //WordModel 배열 생성
+            let wordModels: [WordModel] = wordList.map { word in
+                WordModel(
+                    wordEng: word.wordEng,
+                    meanKor: word.meanKor,
+                    exampleEng: word.exampleEng,
+                    exampleKor: word.exampleKor,
+                    isDone: false,
+                    option1: "22", //입시값
+                    option2: "33" //임시값
+                )
+            }
+            
+            let star = StarModel(
+                title: fileName,
+                words: wordModels,
+                progress: 0
+            )
+            
+            //SwiftData에 저장
+            context.insert(star)
+            logger.info("Inserted StarModel: \(fileName) with \(wordModels.count) words.")
+            
+            //상세 로그 출력
+//            print("[\(fileName)] 로드 완료")
+//            print("총 단어 개수: \(wordModels.count)")
+//            for (index, word) in wordModels.enumerated() {
+//                print("""
+//                      \(index + 1). \(word.wordEng)
+//                         의미: \(word.meanKor)
+//                         예문(ENG): \(word.exampleEng)
+//                         예문(KOR): \(word.exampleKor)
+//                      """)
+//            }
+//            
+//            print("\(fileName) 데이터를 성공적으로 로드했습니다.")
+//            print("Star Title: \(star.title), Words Count: \(star.words.count)")
+            
+            try context.save()
+            
+        } catch {
+            print("\(fileName) 데이터 로드 실패: \(error)")
+        }
+    }
+    
+    struct WordJSON: Codable {
+        let wordEng: String
+        let meanKor: String
+        let exampleEng: String
+        let exampleKor: String
+    }
+    
+    
+    
+}
