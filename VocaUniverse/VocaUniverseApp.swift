fe94a97a8d27489301700eefef0b28a13d787fb5
HwangJeha <155410554+Jeha0101@users.noreply.github.com>
2025-08-23 11:53:09 +0900

    initial commit

---
+++ VocaUniverse/VocaUniverseApp.swift
@@ -0,0 +1,17 @@
+//
+//  VocaUniverseApp.swift
+//  VocaUniverse
+//
+//  Created by 제하맥 on 8/23/25.
+//
+
+import SwiftUI
+
+@main
+struct VocaUniverseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
